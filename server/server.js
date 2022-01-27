08aaa0ffa3421728ae2ce3caa3d2d9c742aa431b
mfbernasol <mfbernasol@gmail.com>
2022-01-27 14:01:34 -0600

    setup routes, db connection

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const routes = require('./routes')
 
 const app = express();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb+srv://mfbern:2160p@cluster0.hcemo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {})
-  }catch(err) {
-    console.log(err.message)
-    process.exit(1)
-  }
-}
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb+srv://mfbern:2160p@cluster0.hcemo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {})
+    
+//   }catch(err) {
+//     console.log(err.message)
+//     process.exit(1)
+//   }
+// }
+
+mongoose
+	.connect("mongodb+srv://mbern:2160p@cluster0.hcemo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true })
+	.then(() => {
+
+		app.use(express.json()) // new
+		app.use("/api", routes)
+
+	})
 
 app.get('/api', (req, res) => {
   res.send("It's working!");
