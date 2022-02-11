9f921460d82c79602056d97bffeec83525fdc4a4
mfbernasol <mfbernasol@gmail.com>
2022-02-10 18:36:31 -0600

    add message

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_URL, { useNewURLParser: true });
@@ -9,11 +9,12 @@ const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Connected to Database'));
 
-app.use(express.json())
-app.use(cors())
-const postsRouter = require('./routes/posts')
-app.use('/posts',postsRouter)
+app.use(express.json());
+app.use(cors());
+const postsRouter = require('./routes/posts');
+app.use('/posts', postsRouter);
 
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the Cheeri API' }));
 
 const PORT = process.env.PORT || 4000;
 
