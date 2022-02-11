a6a7770ff4124fcc8c41ae3acfa08f793b7ff7eb
mfbernasol <mfbernasol@gmail.com>
2022-02-10 18:26:19 -0600

    deploy api

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,31 @@ function App() {
   const [name, setName] = useState('');
 
   //fetch data from API
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios('http://localhost:4000/posts');
-      const sortedResults = result.data;
-      //sort results by date in descending order
-      sortedResults.sort((a, b) => {
-        return new Date(b.createdAt) - new Date(a.createdAt);
-      });
-      setPost(sortedResults);
-    };
+  const fetchData = async () => {
+    const result = await axios('http://localhost:4000/posts');
+    const sortedResults = result.data;
+    //sort results by date in descending order
+    sortedResults.sort((a, b) => {
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    });
+    setPost(sortedResults);
+    console.log(sortedResults);
+  };
 
+  useEffect(() => {
     fetchData();
   }, []);
 
   //ADD data to API
   let handleSubmit = async (e) => {
     e.preventDefault();
+
     const newPost = { name: name, content: message };
     try {
       await axios.post('http://localhost:4000/posts', newPost);
 
-      setPost([newPost,...post ]);
+      setPost([newPost, ...post]);
+      fetchData();
       setName('');
       setMessage('');
     } catch (err) {
@@ -37,8 +40,9 @@ function App() {
   };
 
   return (
-    <div>
-      <h1 className='text-center text-3xl'>Cheeri</h1>
+    <div className=''>
+      <h1 className='text-center text-2xl font-semibold'>cheeri </h1>
+      <h2 className='text-center'>a little place to share some good 😄 </h2>
       <form>
         <div className=' bg-transparent flex items-center'>
           <div className='container mx-auto max-w-md hover:shadow-lg transition duration-300'>
@@ -48,25 +52,29 @@ function App() {
                   className='mr-4 text-gray-700 font-bold inline-block mb-2'
                   htmlFor='name'
                 >
-                  Name (be annonymous!)
+                  Name (be annonymous)
                 </label>
                 <input
                   type='text'
                   className='border bg-gray-100 py-2 px-4 w-full md:w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded'
+                  maxLength={20}
                   value={name ?? ''}
                   placeholder='Your name'
                   onChange={(e) => setName(e.target.value)}
                 />
               </div>
               <label className='label'>
-                <span className='label-text'>Your Message!</span>
+                <span className='label-text font-semibold'>
+                  share something good that happened to you
+                </span>
               </label>
               <textarea
                 className='textarea textarea-bordered w-full md:w-96'
+                maxLength={300}
                 type='text'
                 name='message'
                 value={message}
-                placeholder='Enter you message'
+                placeholder='Your post'
                 onChange={(e) => setMessage(e.target.value)}
               ></textarea>
               <button
@@ -83,15 +91,14 @@ function App() {
       {/* Display Posts */}
       <div>
         <ul>
-          {post.map((data) => (
-            <li key={data._id} className='mt-5 mb-5'>
+          {post.map((data,id) => (
+            <li key={id} className='mt-5 mb-5'>
               <PostCard
                 name={data.name}
                 content={data.content}
                 date={data.createdAt}
-              /> 
+              />
             </li>
-          
           ))}
         </ul>
       </div>
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PostCard = ({ name, content,date }) => {
   return (
     <div className=''>
-      <div className='relative grid grid-cols-1 gap-4 p-4 mb-8 border-rounded-xl bg-white shadow-md'>
+      <div className='relative grid grid-cols-1 gap-4 p-4 mb-8 mx-2 md:mx-48 rounded-xl border-2  bg-white shadow-md'>
         <div className='relative flex gap-4'>
           <div className='flex flex-col w-full'>
             <div className='flex flex-row justify-between'>
@@ -14,7 +14,7 @@ const PostCard = ({ name, content,date }) => {
                 <i className='fa-solid fa-trash'></i>
               </a>
             </div>
-            <p className='text-gray-400 text-sm'>{new Date(date).toLocaleString()}</p>
+            <p className='text-gray-400 text-sm'>{date}</p>
           </div>
         </div>
         <p className='-mt-4 text-gray-500'>{content}</p>
